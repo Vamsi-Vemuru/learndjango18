64aba1caa9884c464acd1c1666195f77fcc0919f
Vamsavardan Vemuru <vamsi0493@gmail.com>
2015-11-17 19:54:23 +0530

    Form in a View

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 
 def home(request):
 	template = 'home.html'
-	title = "My Title %s" % (request.user)
+	title = "Welcome"
+
+	form = SignUpForm(request.POST or None)
 	context = {
 		"template_title" : title,
+		"form" : form
 	}
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		context = {
+			"template_title" : "Thank You"
+		}
+	
 	return render(request, template, context)
